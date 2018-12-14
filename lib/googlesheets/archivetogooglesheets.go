5ad00a3bb98440d8dbd487b1f35fc9e2a14a3b5b
Unknown <baggerone@gmail.com>
2018-12-14 08:17:23 -0500

    Initial success

---
+++ lib/googlesheets/archivetogooglesheets.go
@@ -0,0 +1,194 @@
+package googlesheets
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
+	"strings"
+	"github.com/silinternational/nodeping-cli/lib"
+)
+
+
+const CredentialsForGoogle = `./lib/googlesheets/auth.json`
+
+
+
+func EnsureSheetExists(spreadsheetID, title string, srv *sheets.Service) (int64, error) {
+	doesSheetExist, sheetID, err := GetSheetIDFromTitle(spreadsheetID, title, srv)
+	if err != nil {
+		return 0, err
+	}
+
+	if !doesSheetExist {
+		request := sheets.Request{
+			AddSheet: &sheets.AddSheetRequest{
+				Properties: &sheets.SheetProperties{
+					Title: title,
+				},
+			},
+		}
+
+		rbb := &sheets.BatchUpdateSpreadsheetRequest{
+			Requests: []*sheets.Request{&request},
+		}
+		_, err := srv.Spreadsheets.BatchUpdate(spreadsheetID, rbb).Context(context.Background()).Do()
+		if err != nil {
+			return 0, fmt.Errorf("Unable to create new sheet %s. %s", title, err)
+		}
+	}
+
+	doesSheetExist, sheetID, err = GetSheetIDFromTitle(spreadsheetID, title, srv)
+	if err != nil {
+		return 0, fmt.Errorf("Error finding newly created sheet %s. %v", title, err)
+	}
+
+	if ! doesSheetExist {
+		return 0, fmt.Errorf("Unable to find newly created sheet %s.", title)
+	}
+
+	return sheetID, nil
+}
+
+
+func EnsureMonthColumnExists(sheetID int64, month, year, spreadsheetID string, srv *sheets.Service) (int, error) {
+
+	desiredMonthPosition, err := GetMonthPosition(month)
+	monthHeader := fmt.Sprintf("%s %s", month, year)
+
+	monthsRange := fmt.Sprintf("%s!B2:Z2", year)
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, monthsRange).Do()
+	if err != nil {
+		return 0, fmt.Errorf("Error getting month headings for %s: %s", monthsRange, err)
+	}
+
+	if len(resp.Values) < 1 {
+		return 0, fmt.Errorf("Error getting month headings for %s. Make sure there is a month heading in B2", year)
+	}
+
+	indexOfFirstMonth := 1
+	chosenColumn := 0
+	lastIndex := 0
+	for index, value := range resp.Values[0] {
+		lastIndex = index
+		columnHeader := fmt.Sprintf("%v", value)
+
+		if columnHeader == "" {
+			chosenColumn = index + indexOfFirstMonth
+			break
+		}
+
+		colMonthPosition, err := GetMonthPosition(columnHeader)
+		if err != nil {
+			continue
+		}
+		if desiredMonthPosition < colMonthPosition {
+			chosenColumn = index + indexOfFirstMonth
+			InsertColumn(int64(chosenColumn), sheetID, spreadsheetID, srv)
+			err := WriteToCellWithColumnIndex(2, int64(chosenColumn), monthHeader, year, spreadsheetID, srv)
+			return chosenColumn, err
+		}
+	}
+
+	if chosenColumn == 0 {
+		AddColumn(sheetID, spreadsheetID, srv)
+		chosenColumn = lastIndex + indexOfFirstMonth + 1
+	}
+
+	err = WriteToCellWithColumnIndex(2, int64(chosenColumn), monthHeader, year, spreadsheetID, srv)
+	return chosenColumn, err
+}
+
+
+func EnsureCheckRowExists(sheetID int64, nodepingCheck, year, spreadsheetID string, srv *sheets.Service) (int, error) {
+	checksRange := fmt.Sprintf("%s!A3:A100", year)
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, checksRange).Do()
+	if err != nil {
+		return 0, fmt.Errorf("Error getting Nodeping Check names for %s: %s", nodepingCheck, err)
+	}
+
+	if len(resp.Values) < 1 {
+		return 0, fmt.Errorf("Error getting Nodeping Check names for %s. Make sure there is a name in A3 or below", year)
+	}
+
+	npCheckLower := strings.ToLower(nodepingCheck)
+    indexOfFirstCheck := 3
+
+	chosenRow := 0
+	lastIndex := 0
+
+	for index, value := range resp.Values {
+		lastIndex = index
+
+		if len(value) < 1 {
+			chosenRow = index + indexOfFirstCheck
+			break
+		}
+
+		rowCheckName := fmt.Sprintf("%v", value[0])
+
+		if npCheckLower < strings.ToLower(rowCheckName) {
+			chosenRow = index + indexOfFirstCheck - 1  // It must be doing an "insert below"
+			InsertRow(int64(chosenRow), sheetID, spreadsheetID, srv)
+			chosenRow += 1
+			err := WriteToCellWithColumnLetter(int64(chosenRow), "A", nodepingCheck, year, spreadsheetID, srv)
+			return chosenRow, err
+		} else if npCheckLower == strings.ToLower(rowCheckName) {
+			chosenRow = index +indexOfFirstCheck
+			return chosenRow, nil
+		}
+	}
+
+	if chosenRow == 0 {
+		AddColumn(sheetID, spreadsheetID, srv)
+		chosenRow = lastIndex + indexOfFirstCheck + 1
+	}
+
+	err = WriteToCellWithColumnLetter(int64(chosenRow), "A", nodepingCheck, year, spreadsheetID, srv)
+	return chosenRow, err
+}
+
+func ArchiveResultsForMonth(contactGroupName, month, year, spreadsheetID, nodePingToken string) {
+	credBytes, err := ioutil.ReadFile(CredentialsForGoogle)
+	if err != nil {
+		log.Fatalf("Unable to read google credentials file: %v", err)
+	}
+
+	config, err := google.JWTConfigFromJSON(credBytes, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Fatalf("Unable to parse google credentials file to config: %v", err)
+	}
+	client := config.Client(context.Background())
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	sheetID, err := EnsureSheetExists(spreadsheetID, year, srv)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+
+	uptimeResults, err := lib.GetUptimesForContactGroup(nodePingToken, contactGroupName, "LastMonth")
+	if err != nil {
+		log.Fatalf("Error getting Nodeping results.  %v", err)
+	}
+
+	monthColumn, err := EnsureMonthColumnExists(sheetID, month, year, spreadsheetID, srv)
+	if err != nil {
+		log.Fatalf("Error choosing column for %s.  %v", month, err)
+	}
+
+	for nodepingCheck, percentage := range uptimeResults.Uptimes {
+		checkRow, err := EnsureCheckRowExists(sheetID, nodepingCheck, year, spreadsheetID, srv)
+		if err != nil {
+			log.Fatalf("Error adding row for %s", nodepingCheck)
+		}
+
+		err = WriteToCellWithColumnIndex(int64(checkRow), int64(monthColumn), fmt.Sprintf("%.3f", percentage), year, spreadsheetID, srv)
+	}
+}
---
+++ lib/googlesheets/basicoperations.go
@@ -0,0 +1,150 @@
+package googlesheets
+
+import (
+	"strings"
+	"fmt"
+	"google.golang.org/api/sheets/v4"
+	"golang.org/x/net/context"
+	"log"
+)
+
+func GetMonthPosition(monthLabel string) (int, error) {
+	monthLabel = strings.Trim(monthLabel, " ")
+	monthParts := strings.Split(monthLabel, " ")
+	month := strings.ToLower(monthParts[0])
+
+	indexes := map[string]int{
+		"january": 1,
+		"february": 2,
+		"march": 3,
+		"april": 4,
+		"may": 5,
+		"june": 6,
+		"july": 7,
+		"august": 8,
+		"september": 9,
+		"october": 10,
+		"november": 11,
+		"december": 12,
+	}
+
+	index, ok := indexes[month]
+	if ! ok {
+		return 0, fmt.Errorf("Month %s not valid", monthLabel)
+	}
+
+	return index, nil
+}
+
+func ConvertColumnIndexToLetter(index int64) (string, error) {
+
+	if index > 25 {
+		return "", fmt.Errorf("Not allowed to convert index if over 25. It was %d", index)
+	}
+
+	runeA := int64([]rune("A")[0])
+
+	return fmt.Sprintf("%c", index + runeA), nil
+}
+
+
+func WriteToCellWithColumnLetter(rowIndex int64, columnLetter, newValue, sheetName, spreadsheetID string, srv *sheets.Service) error {
+	cellRange := fmt.Sprintf("%s!%s%d", sheetName, columnLetter, rowIndex)
+	valueRange := &sheets.ValueRange{}
+
+	updateValue := []interface{}{newValue}
+	valueRange.Values = append(valueRange.Values, updateValue)
+
+	_, err := srv.Spreadsheets.Values.Update(spreadsheetID, cellRange, valueRange).ValueInputOption("RAW").Do()
+	if err != nil {
+		return fmt.Errorf("Unable to write to cell %s. %v", cellRange, err)
+	}
+
+	return nil
+}
+
+
+func WriteToCellWithColumnIndex(rowIndex, columnIndex int64, newValue, sheetName, spreadsheetID string, srv *sheets.Service) error {
+	columnLetter, err := ConvertColumnIndexToLetter(columnIndex)
+	if err != nil {
+		return err
+	}
+	return WriteToCellWithColumnLetter(rowIndex, columnLetter, newValue, sheetName, spreadsheetID, srv)
+}
+
+
+func InsertRowOrColumn(insertRowNotColumn bool, index, sheetID int64, spreadsheetID string, srv *sheets.Service) error {
+	dimension := "COLUMNS"
+	if insertRowNotColumn {
+		dimension = "ROWS"
+	}
+
+	request := sheets.Request{
+		InsertDimension: &sheets.InsertDimensionRequest {
+			Range: &sheets.DimensionRange {
+				SheetId: sheetID,
+				Dimension: dimension,
+				StartIndex: index,
+				EndIndex: index+1,
+			},
+			InheritFromBefore: true,
+		},
+	}
+
+	rbb := &sheets.BatchUpdateSpreadsheetRequest{
+		Requests: []*sheets.Request{&request},
+	}
+	_, err := srv.Spreadsheets.BatchUpdate(spreadsheetID, rbb).Context(context.Background()).Do()
+	if err != nil {
+		return fmt.Errorf("Unable to insert %s %d. %s", dimension, index, err)
+	}
+
+	return nil
+}
+
+
+func InsertColumn(index, sheetID int64, spreadsheetID string, srv *sheets.Service) error {
+	return InsertRowOrColumn(false, index, sheetID, spreadsheetID, srv)
+}
+
+
+func InsertRow(index, sheetID int64, spreadsheetID string, srv *sheets.Service) error {
+	return InsertRowOrColumn(true, index, sheetID, spreadsheetID, srv)
+}
+
+
+
+func AddColumn(sheetID int64, spreadsheetID string, srv *sheets.Service) {
+	request := sheets.Request{
+		AppendDimension: &sheets.AppendDimensionRequest {
+			Dimension: "COLUMNS",
+			Length: 1,
+			SheetId: sheetID,
+		},
+	}
+
+	rbb := &sheets.BatchUpdateSpreadsheetRequest{
+		Requests: []*sheets.Request{&request},
+	}
+	_, err := srv.Spreadsheets.BatchUpdate(spreadsheetID, rbb).Context(context.Background()).Do()
+	if err != nil {
+		log.Fatalf("Unable to add column to sheet %d. %s", sheetID,  err)
+	}
+}
+
+
+func GetSheetIDFromTitle(spreadsheetID, title string, srv *sheets.Service) (bool, int64, error) {
+	ssResp, err := srv.Spreadsheets.Get(spreadsheetID).Do()
+
+	if err != nil {
+		return false, 0, fmt.Errorf("Error trying to find sheet %s. %v", title, err)
+	}
+
+	for _, next := range ssResp.Sheets {
+		if next.Properties.Title == title {
+			return true, next.Properties.SheetId, nil
+		}
+	}
+
+	return false, 0, nil
+}
