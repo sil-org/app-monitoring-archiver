8f2f8042198e0e011854b69c022e3659d8203429
briskt <3172830+briskt@users.noreply.github.com>
2023-11-17 15:57:09 -0700

    use custom runtime provided.al2 in advance of go1.x deprecation

--- codeship/build.sh
+++ codeship/build.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-# Exit script with error if any step fails.
-set -e
-
 # Echo out all commands for monitoring progress
 set -x
 
+# When using the provided.al2 runtime, the binary must be named "bootstrap" and be in the root directory
 if [ "$1" = "cli" ]
 then
-  go build -ldflags="-s -w" -o bin/cli cmd/cli/main.go
+  CGO_ENABLED=0 go build -tags lambda.norpc -ldflags="-s -w" -o bootstrap cmd/cli/main.go
 else
-  go build -ldflags="-s -w" -o bin/lambda cmd/lambda/main.go
+  CGO_ENABLED=0 go build -tags lambda.norpc -ldflags="-s -w" -o bootstrap cmd/lambda/main.go
 fi
