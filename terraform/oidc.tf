6b1948fc70053124bdb42780a1b924b904b724cb
Praveen <praveen_koilraj@sil.org>
2026-02-06 16:23:41 +0530

    update AWS provider version and remove access key variables; add OIDC role and policy for Terraform Cloud

---
+++ terraform/oidc.tf
@@ -0,0 +1,78 @@
+/*
+ * App Monitoring Archiver Hashicorp Terraform Role
+ */
+resource "aws_iam_role" "hcp" {
+  description = "for Terraform workspace app-monitoring-archiver"
+  name        = "app-monitoring-archiver-hcp-terraform"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::369020531563:oidc-provider/app.terraform.io"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "app.terraform.io:aud" = "aws.workload.identity"
+          }
+          "ForAnyValue:StringLike" = {
+            "app.terraform.io:sub" = [
+              "organization:gtis:project:*:workspace:app-monitoring-archiver:run_phase:*",
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "hcp" {
+  name = "Deployment"
+  role = aws_iam_role.hcp.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "iam:CreateUser",
+          "iam:DeleteUser",
+          "iam:GetUser",
+          "iam:TagUser",
+          "iam:UntagUser",
+          "iam:ListUserTags",
+          "iam:CreateAccessKey",
+          "iam:DeleteAccessKey",
+          "iam:GetAccessKeyLastUsed",
+          "iam:ListAccessKeys",
+          "iam:CreatePolicy",
+          "iam:DeletePolicy",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:ListPolicyVersions",
+          "iam:CreatePolicyVersion",
+          "iam:DeletePolicyVersion",
+          "iam:AttachUserPolicy",
+          "iam:DetachUserPolicy",
+          "iam:ListAttachedUserPolicies",
+          "iam:ListUserPolicies",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:iam::369020531563:user/app-monitoring-archiver-*"
+        Sid      = "IAMManagement"
+      },
+      {
+        Action = [
+          "iam:GetRole",
+          "iam:GetRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:iam::369020531563:role/app-monitoring-archiver-hcp-terraform"
+        Sid      = "RoleSelfRead"
+      },
+    ]
+  })
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,8 @@ output "cdk_secret_access_key" {
   value     = aws_iam_access_key.cdk.secret
   sensitive = true
 }
+
+output "tfc_role_arn" {
+  description = "ARN of the IAM role for Terraform Cloud OIDC authentication"
+  value       = aws_iam_role.hcp.arn
+}
