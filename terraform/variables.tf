bedfad90c5e92905595947854de5d5c06ce2b32c
Praveen <praveen_koilraj@sil.org>
2026-02-23 16:31:43 +0530

    add AWS access key and secret key variables to Terraform configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,14 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "aws_access_key" {
+  default = null
+}
+
+variable "aws_secret_key" {
+  default = null
+}
+
 /*
  * AWS tag values
  */
