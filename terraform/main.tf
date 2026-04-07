b8ed4ded661b87fbc76a5c01d7ca3918a5304355
briskt <3172830+briskt@users.noreply.github.com>
2026-04-07 10:18:48 +0800

    Use a role for deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,39 @@
-/*
- * Create IAM user for CDK
- */
-resource "aws_iam_user" "cdk" {
-  name = "${var.app_name}-cdk"
-}
+# Role for Continuous Deployment using CDK
 
-/*
- * Create Access Key/Secret for user
- */
-resource "aws_iam_access_key" "cdk" {
-  user = aws_iam_user.cdk.name
-}
-
-/*
- * Create IAM policy
- */
-resource "aws_iam_policy" "cdk" {
-  name        = "${var.app_name}-cdk"
-  description = "CDK deployment policy"
-
-  policy = jsonencode({
+resource "aws_iam_role" "cd" {
+  description = "for GitHub Actions to deploy sil-org/idp-support-bot"
+  name        = "${var.app_name}-cd"
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "sts:AssumeRole",
-        ]
-        Resource = [
-          "arn:aws:iam::*:role/cdk-*",
-        ]
-      },
-    ]
+    Statement = [{
+      Sid    = "GitHub"
+      Effect = "Allow"
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Principal = {
+        Federated = var.github_oidc_provider_arn
+      }
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+        },
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" : "repo:${var.github_repository}:*"
+        }
+      }
+    }]
   })
 }
 
-/*
- * Attach policy to user
- */
-resource "aws_iam_user_policy_attachment" "cdk" {
-  user       = aws_iam_user.cdk.name
-  policy_arn = aws_iam_policy.cdk.arn
+resource "aws_iam_role_policy" "cd" {
+  name = "${var.app_name}-cd"
+  role = aws_iam_role.cd.name
+
+  policy = jsonencode({
+    "Version" : "2012-10-17"
+    "Statement" : [{
+      Effect   = "Allow"
+      Action   = "sts:AssumeRole"
+      Resource = "arn:aws:iam::*:role/cdk-*"
+    }]
+  })
 }
